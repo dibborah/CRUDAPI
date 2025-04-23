ffc3edaf5f3894bb91ca4dbf617d032fb1622013
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-23 22:28:52 +0530

    Done: Create product model + get, put, post request api creation

--- index.js
+++ index.js
@@ -1,15 +1,61 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import mongodbPass from './secret.js';
+import Product from './models/product.model.js';
 
-const app = express()
+const app = express();
+app.use(express.json());
 
-// app.listen(3000, () => {
-//     console.log("Server is listening in port 3000...");
-// })
+// we not allow json to our nodejs by default
 
 app.get('/', (req, res) => {
-    res.send('Just hello world!!!');
+    res.send('Hello from nodejs API updated!!!');
+})
+
+app.get('/api/products', async (req, res) => {
+    try {
+      const products = await Product.find({});
+      res.status(200).json(products)
+    } catch(error) {
+      res.status(500).json({ message: error.message });
+    }
+})
+
+app.get('/api/product/:id', async (req, res) => {
+    try {
+      const products = await Product.findById(req.params.id);
+      res.status(200).json(products)
+    } catch(error) {
+      res.status(500).json({ message: error.message });
+    }
+})
+
+app.post('/api/products', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch(error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+})
+
+// update a product
+app.put('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    
+    if (!product) {
+      return res.status(404).json({ message: "Product not found!!!" });
+    }
+    
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 })
 
 const url = `mongodb+srv://dibborah100:${mongodbPass}@backenddb.duonria.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB`;
@@ -22,5 +68,6 @@ mongoose.connect(url)
     })
   })
   .catch(() => console.log('connnection failure!!!'));
+
 
 
